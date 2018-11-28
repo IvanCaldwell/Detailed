28b5a040ff62e24e0131c431ad6176fce6ab88f5
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-27 16:06:21 -0800

    Review: Project Setup

--- Detailed Project/Detailed Project/TableViewCell.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import UIKit
-
-class TableViewCell: UITableViewCell {
-    static let reuseIdentifier = "cell"
-    @IBOutlet weak var tableViewCellImage: UIImageView!
-}
--- Detailed Review Project/Detailed Review Project/AppDelegate.swift
+++ Detailed Review Project/Detailed Review Project/AppDelegate.swift
@@ -1,8 +1,19 @@
+//
+//  AppDelegate.swift
+//  Detailed Review Project
+//
+//  Created by Ivan Caldwell on 11/27/18.
+//  Copyright © 2018 Ivan Caldwell. All rights reserved.
+//
+
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
     var window: UIWindow?
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
